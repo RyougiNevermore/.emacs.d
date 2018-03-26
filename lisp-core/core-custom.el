a2040953275169ad2a1654ceed1355d3481113bb
RyougiNevermore <ryougi.nevermore@hotmail.com>
2018-03-26 14:54:13 +0800

    format core-const, core-custom, core-package el files.

--- lisp-core/core-custom.el
+++ lisp-core/core-custom.el
@@ -18,79 +18,93 @@
 ;; conf
 (defgroup my nil
   "Personal Emacs configurations."
-  :group 'extensions)
+  :group 'extensions
+)
 
 ;; name
 (defcustom my-full-name "ryougi"
   "Set user full name."
-  :type 'string)
+  :type 'string
+)
 
 ;; email
 (defcustom my-mail-address "ryougi.nevermore@hotmail.com"
   "Set user email address."
-  :type 'string)
+  :type 'string
+)
 
 ;; melpa
 (defcustom my-package-archives 'melpa
   "Set package archives from which to fetch."
-  :type '(choice
-          (const :tag "Melpa" melpa)
-          (const :tag "Emacs-China" emacs-china)
-          (const :tag "Tuna" tuna)))
+  :type '(choice 
+                  (const :tag "Melpa" melpa)
+                  (const :tag "Emacs-China" emacs-china)
+                  (const :tag "Tuna" tuna)
+                )
+)
 
 ;; emoji
 (defcustom my-emoji-enabled nil
   "Enable emoji features or not."
-  :type 'boolean)
+  :type 'boolean
+)
 
 ;; benchmark
 (defcustom my-benchmark-enabled nil
   "Enable the init benchmark or not."
-  :type 'boolean)
+  :type 'boolean
+)
 
 ;; my-org-agenda-dir
 (defcustom my-org-agenda-dir "~/Org"
   "Set Org agenda dir."
-  :type 'string)
+  :type 'string
+)
 
 ;; osx key mode
 (defcustom osx-key-mode nil
   "Use osx-key-mode."
-  :type 'boolean)
+  :type 'boolean
+)
 
 ;; swapper Meta(Alt) and Super(Win) key for gnu
 (defcustom gnu-swapper-meta-super-key nil
   "Swapper Meta(Alt) and Super(Win) key for gnu."
-  :type 'boolean)
+  :type 'boolean
+)
 
 ;; swapper Meta(Alt) and Super(Win) key for windows(Cygwin)
 (defcustom win-swapper-meta-super-key nil
   "Swapper Meta(Alt) and Super(Win) key for windows(Cygwin)."
-  :type 'boolean)
+  :type 'boolean
+)
 
 ;; theme
 (defcustom my-theme 'default
   "Set color theme."
   :type '(choice
-          (const :tag "Default theme(challenger-deep)" default)
-          (const :tag "Default theme(dracula)" dracula)
-          (const :tag "Dark theme(spacemacs-dark)" spacemacs-dark)
-          (const :tag "Light theme(spacemacs-light)" spacemacs-light)
-          (const :tag "Daylight theme(leuven)" spacemacs-daylight)))
+                  (const :tag "Default theme(challenger-deep)" default)
+                  (const :tag "Default theme(dracula)" dracula)
+                  (const :tag "Dark theme(spacemacs-dark)" spacemacs-dark)
+                  (const :tag "Light theme(spacemacs-light)" spacemacs-light)
+                  (const :tag "Daylight theme(leuven)" spacemacs-daylight)
+                )  
+)
 
 ;; For Emacs devel
 (when (= emacs-minor-version 0)
   (setq package-user-dir (locate-user-emacs-file "elpa-devel"))
   (setq desktop-base-file-name ".emacs-devel.desktop")
-  (setq desktop-base-lock-name ".emacs-devel.desktop.lock"))
+  (setq desktop-base-lock-name ".emacs-devel.desktop.lock")
+)
 
 ;; user-init.el
 (setq user-init-file (expand-file-name "user-init.el" user-emacs-directory))
-(if (file-exists-p user-init-file)
-    (load user-init-file))
+(if (file-exists-p user-init-file) 
+  (load user-init-file)
+)
 
 (provide 'core-custom)
-
 
 ;;----------------------------------------------------------------------------
 ;; core-custom.el ends here
--- lisp-core/core-package.el
+++ lisp-core/core-package.el
@@ -17,54 +17,70 @@
 
 (eval-when-compile (require 'core-custom))
 
-
 (with-eval-after-load 'package
   (defun package--save-selected-packages (&optional value)
     "Set and (don't!) save `package-selected-packages' to VALUE."
     (when value
-      (setq package-selected-packages value))
+      (setq package-selected-packages value)
+    )
     (unless after-init-time
-      (add-hook 'after-init-hook #'package--save-selected-packages))))
-
+      (add-hook 'after-init-hook #'package--save-selected-packages)
+    )
+  )
+)
 
 ;; ELPA
 (cond
  ((eq my-package-archives 'melpa)
-  (setq package-archives '(("gnu"   . "https://elpa.gnu.org/packages/")
-                           ("melpa" . "https://melpa.org/packages/")
-			   ("melpa-stable" . "https://stable.melpa.org/packages/"))))
+    (setq package-archives '(
+        ("gnu"   . "https://elpa.gnu.org/packages/")
+        ("melpa" . "https://melpa.org/packages/")
+        ("melpa-stable" . "https://stable.melpa.org/packages/")
+      )
+    )
+  )
  ((eq my-package-archives 'emacs-china)
-  (setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
-                           ("melpa" . "http://elpa.emacs-china.org/melpa/")
-			   ("melpa-stable" . "http://elpa.emacs-china.org/melpa-stable/"))))
+  (setq package-archives '(
+        ("gnu"   . "http://elpa.emacs-china.org/gnu/")
+        ("melpa" . "http://elpa.emacs-china.org/melpa/")
+        ("melpa-stable" . "http://elpa.emacs-china.org/melpa-stable/")
+      )
+    )
+  )
  ((eq my-package-archives 'tuna)
-  (setq package-archives '(("gnu"   . "https://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
-                           ("melpa" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
-			   ("melpa-stable" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/melpa-stable/")))))
-
-
+  (setq package-archives '(
+        ("gnu"   . "https://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+        ("melpa" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
+        ("melpa-stable" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/melpa-stable/")
+      )
+     )
+  )
+)
 
 ;; Initialize packages
 (require 'package)
-(setq package-enable-at-startup nil)    ; To prevent initialising twice
+;; To prevent initialising twice
+(setq package-enable-at-startup nil)    
 (package-initialize)
 
-
-;; Setup `use-package'
+;; Install use-package
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
-  (package-install 'use-package))
+  (package-install 'use-package)
+)
 
 (eval-when-compile
-  (require 'use-package))
+  (require 'use-package)
+)
 
 (setq use-package-always-ensure t)
 (setq use-package-always-defer t)
 (setq use-package-expand-minimally t)
 (setq use-package-enable-imenu-support t)
 
-;; Required by `use-package'
+;; Install diminish
 (use-package diminish)
+;; Install bind-key
 (use-package bind-key)
 
 ;; Initialization benchmark
@@ -72,7 +88,9 @@
   (use-package benchmark-init
     :init
     (benchmark-init/activate)
-    (add-hook 'after-init-hook #'benchmark-init/deactivate)))
+    (add-hook 'after-init-hook #'benchmark-init/deactivate)
+  )
+)
 
 (provide 'core-package)
 
